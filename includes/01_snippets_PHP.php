690b7c4c396fab8ce9420dbd6dd8390a8458afa3
webmasterpf-DG <webmaster@provence-formation.com>
2018-03-21 13:56:03 +0100

    Starter S3: Contenu Europe : layout et style (4) - Liste docs joints PHP fonctionne, ajout de pictos PNG

--- includes/01_snippets_PHP.php
+++ includes/01_snippets_PHP.php
@@ -3,21 +3,41 @@
 </html>
 
     <?php
-                    /* 06/2015 Liste les fichiers à partir du tableau créé
+                    /* 03/2018 Liste les fichiers uploadés par
+                     * filefield and linkfield
                      * création d'une liste dynamique 
                      */
-                    if (!empty($node->field_NAME[0]['view'])): ?>
+                    if (!empty(
+                        
+         $node->field_NAME[0]['view']
+        OR $node->field_NAME2[0]['view']
+    ) ): ?>
                 <div class="CLASS_NAME">
                <?php  //récupération du nom du champ
                print $node->content['field_NAME']['field']['#title'] ?>
                     <ul>
                <?php
-               foreach ($node->field_NAME as $key => $lien) {
-                    print '<li>'.$node->field_NAME[$key]['view'].'</li>';
+               //Parcourir le tableau field_NAME de l'objet $node pour lister le contenu clé/valeur
+               foreach ($node->field_NAME as $key => $fichiers) {
+                   //Ne pas afficher les champs vides du tableau
+                      if (!empty($fichiers['view'])):
+//                       print 'Contenu de $fichier'.var_dump($fichier);
+
+                       print  '<li>'.$node->field_NAME[$key]['view'].'</li>';
+
+                endif;
+               }
+               foreach ($node->field_NAME2 as $key => $liens) {
+                   //Ne pas afficher les champs vides du tableau
+                      if (!empty($liens['view'])):
+                    print '<li>'.$node->field_NAME2[$key]['view'].'</li>';
+                endif;
                }
                ?>  
                   </ul>   </div>
             <?php endif;?>
+
+
 
 <?php //mise en forme tableau (à finaliser)
 if (!empty($node->field_NAME[0]['view'])): ?>
--- includes/dedicates_inc/inc_docs_rub_international.php
+++ includes/dedicates_inc/inc_docs_rub_international.php
@@ -1,12 +1,6 @@
-<?php
-/*
- * Liste dynamique pour afficher les docs joints
- * de nombre.
- */
-?>
-
     <?php
-                    /* 06/2015 Liste les fichiers à partir du tableau créé
+                    /* 03/2018 Liste les fichiers uploadés par
+                     * filefield and linkfield
                      * création d'une liste dynamique 
                      */
                     if (!empty(
@@ -19,8 +13,21 @@
                print $node->content['field_NAME']['field']['#title'] ?>
                     <ul>
                <?php
-               foreach ($node->field_fichier_joint as $key => $lien) {
-                    print '<li>'.$node->field_fichier_joint[$key]['view'].'</li>';
+               //Parcourir le tableau field_NAME de l'objet $node pour lister le contenu clé/valeur
+               foreach ($node->field_fichier_joint as $key => $fichiers) {
+                   //Ne pas afficher les champs vides du tableau
+                      if (!empty($fichiers['view'])):
+//                       print 'Contenu de $fichier'.var_dump($fichier);
+
+                       print  '<li>'.$node->field_fichier_joint[$key]['view'].'</li>';
+
+                endif;
+               }
+               foreach ($node->field_lien_site as $key => $liens) {
+                   //Ne pas afficher les champs vides du tableau
+                      if (!empty($liens['view'])):
+                    print '<li>'.$node->field_lien_site[$key]['view'].'</li>';
+                endif;
                }
                ?>  
                   </ul>   </div>
