99e45728edd824dcad01ac0fcdec4efe2780bc46
webmasterpf-DG <webmaster@provence-formation.com>
2018-04-06 12:33:38 +0200

    Starter S3: Page Actualités > tpl

--- node/node-page_actualites.tpl.php
+++ node/node-page_actualites.tpl.php
@@ -1,76 +1,66 @@
 <?php
-/* Template pour la page actualité - redesign façon newspaper/magazine
- * Start Juillet 2011
- */?>
-<!-- ************ NODE TEMPLATE PAGE ACTU REDESIGN ****************** -->
+/* Ce template permet la création d'un layout multicolonne pour les pages de base, en permettant la disposition facile
+ * des champs CCK custom, si nécessaires pour une page de base.
+*/?>
+<!--______________NODE TPL POUR PAGE.TPL CUSTOM - PAGE ACTUALITES ________________ -->
 <div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
-  <div class="node-inner">
-      <?php if (!$page): ?>
-      <h2 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
-<!-- ______________________ COLONNE REVUE PRESSE _______________________ -->
-<div class="col_375" id="rp_actu_gloable">
-         <?php
- /* inclusion vue liste illustrée */
-    include (drupal_get_path('theme','cyrano_pf').'/includes/inc_vue_actu_globale_rp.php');
+    <div class="node-inner">
+    <!--______________ ILLUSTRATION et TITRE ________________ -->
+        <div id="zone-illustration" class="page-actualites">
+                     <?php if ($title): /*copier le titre dans la zone desirée*/?>
+         
+            <?php endif; ?>
+            
+             <?php if ($node->field_illus_administratif[0]['view']
+                 OR
+                 $title
+                 ): ?>
+            <div id="illustration-pleinepage" class="illustration-espace">
+                   <h1 class="titre_overlay titre_page"><?php print $title; ?></h1>
+                 <?php  print $node->field_illus_administratif[0]['view'] ?>
+            </div>
+            <?php endif;?>
+     
+            
+            </div><!-- /zone-illustration -->
+             <!--______________ ZONE 1 ________________ -->
+        <div id="zone-1" class="zone_layout_max_centre page-actualites">
+          <?php print $picture; ?>
+
+<?php if ($submitted): ?>
+                <span class="submitted"><?php print $submitted; ?></span>
+<?php endif; ?>
+                
+                       <?php if (!empty($node->content['body']['#value'])):
+     print '<div class="content">'.$node->content['body']['#value'].'</div>';
+     endif;
+               ?> 
+                
+      <?php
+              global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_zone_1.php');
               ?>
-    <br clear="all"/>
-   <?php
-   include (drupal_get_path('theme','cyrano_pf').'/includes/inc_vue_actu_globale_rp_petit.php');
-   ?>
-     <?php
- /* inclusion d'une region pour bloc */
-              include (drupal_get_path('theme','cyrano_pf').'/includes/inc_region_col_G1.php');
-              ?>
-</div>
-<!-- ______________________ COLONNE ACTUS ASSO _______________________ -->
-<div class="col_375" id="actu_asso_actu_globale">
-     <?php
- /* inclusion vue liste illustrée */
-              include (drupal_get_path('theme',cyrano_pf).'/includes/inc_vue_actu_globale_actuasso.php');
-              ?>
-     <br clear="all"/>
-       <?php
- /* inclusion vue liste illustrée petit */
-              include (drupal_get_path('theme',cyrano_pf).'/includes/inc_vue_actu_globale_actuasso_petit.php');
-              ?>
-         <?php
- /* inclusion d'une region pour bloc */
-              include (drupal_get_path('theme',cyrano_pf).'/includes/inc_region_col_G2.php');
-              ?>
-</div>
-<!-- ______________________ COLONNE INFOS FIXES RSS _______________________ -->
-<div class="col_220" id="infos_fixes_actu_globale">
-     <?php
- /* inclusion d'une region pour bloc */
-              include (drupal_get_path('theme',cyrano_pf).'/includes/inc_region_col_G3.php');
-              ?>
-    
-        <div class="content contenu-actu-globale">
-      <?php print $content; ?>
-    </div>
+                
+           
+               
 
-</div>
-<br clear="all"/>
+                        <?php if ($node->field_vue_content_vr[0]['view']):
+     print '<aside class="contenu-rubrique">'.$node->field_vue_content_vr[0]['view'].'</aside>';
+ endif;
+                            ?>
+      
+               
+         
+                   
+        <?php if ($terms): ?>
+        <div class="taxonomy"><?php //print $terms; ?></div>
+        <?php endif;?>
 
-    <?php print $picture; ?>
-
-    <?php if ($submitted): ?>
-      <span class="submitted"><?php print $submitted; ?></span>
-    <?php endif; ?>
+        <?php if ($links): ?>
+        <div class="links"> <?php //print $links; ?></div>
+        <?php endif; ?>
+        </div><!--  /zone-1-->
 
 
-
-    <?php if ($terms): ?>
-
-      <div id="taxonomy">
-<?php print Cyrano_PF_print_terms($node);?>
-      </div>
-    <?php endif;?>
-
-    <?php if ($links): ?> 
-      <div class="links"> <?php print $links; ?></div>
-    <?php endif; ?>
-
-  </div> <!-- /node-inner -->
+    </div> <!-- /node-inner -->
 </div> <!-- /node-->
