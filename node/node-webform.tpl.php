199bd0290e3057cdb30bbdc75f5f76b204c92769
webmasterpf-DG <webmaster@provence-formation.com>
2018-04-18 15:09:47 +0200

    Starter S3: Webform: layout et style de base

--- node/node-webform.tpl.php
+++ node/node-webform.tpl.php
@@ -3,95 +3,66 @@
  * Permet d'avoir un template spécial pour le webform
  * NODE-WEBFORM.TPL GENERIK si besoin possible faire theme pour webform selon node-webform-NID.tpl.php
  */
-
 ?>
-<!-- NODE-WEBFORM.TPL GENERIK -->
+<!--______________NODE TPL POUR PAGE.TPL WEBFORM CLASSIQUE ________________ -->
+
 <div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
-   
-      <!--______________COLONNE 1________________ -->
-        <?php /* choix du layout selon nombre de colonne
-         * .col1_layout_200_590_200{} .col1_layout_330_all{} .col1_layout_18_56_25{}
-         * .col2_layout_200_590_200{} .col2_layout_330_all{} .col2_layout_18_56_25{}
-         * .col3_layout_200_590_200{} .col3_layout_330_all{} .col3_layout_18_56_25{}
-         * .col1_layout_215_535_235{} .col2_layout_215_535_235{} .col3_layout_215_535_235{}
-         * .col1_layout_490_all{}  .col2_layout_490_all{}
-         */?>
-  
-<div id="colonne-1" class="col1_layout_200_590_200">
-     <?php if ($title): /*insertion du titre de la page et style differencié*/?>
-     <h1 class="titre_pl"><?php print $title; ?></h1>
-
-    <?php endif; ?>
-      <br clear="all"/>
-       <?php
-              global $theme_path;
-              include ($theme_path.'/includes/inc_region_col_G1.php');
-              ?>
-</div>
-<!--______________COLONNE 2________________ -->
+     <div class="node-inner">
+        <!--______________ ILLUSTRATION et TITRE ________________ -->
+        <div id="zone-illustration" class="webform">
+                     <?php if ($title): /*copier le titre dans la zone desirée*/?>
          
-        <div id="colonne-2" class="col2_layout_200_590_200">
+            <?php endif; ?>
+            
+                 <?php if ($node->field_illus_administratif[0]['view']
+                 OR
+                 $title
+                 ): ?>
+            <div id="illustration-pleinepage" class="illustration-webform">
+                   <h1 class="titre_overlay titre_page"><?php print $title; ?></h1>
+                 <?php  print $node->field_illus_administratif[0]['view'] ?>
+            </div>
+            <?php endif;?>
+     
+            
+            </div><!-- /zone-illustration -->
+             <!--______________ ZONE 1 ________________ -->
+        <div id="zone-1" class="zone_layout_max_centre webform">
+          <?php print $picture; ?>
 
-     <?php if ($submitted) { ?>
-    <span class="submitted"><?php print $submitted?></span>
-  <?php }; ?>
+<?php if ($submitted): ?>
+                <span class="submitted"><?php print $submitted; ?></span>
+<?php endif; ?>
+                
+                            <?php
+              global $theme_path;
+              include ($theme_path.'/includes/regions_inc/inc_region_zone_1.php');
+              ?>
 
- 
+               <?php if ($node->field_complement_info_formation[0]['view']): ?>
+                    <div class="complement-titre-fiche">
+                        <?php (print $node->field_complement_info_formation[0]['view']); /* Info complementaire sur formation */ ?>
+                    </div>
+                <?php endif; ?>
+                
+                   <?php if ($node->field_intro_fiche_formation[0]['view']): ?>
+                    <div class="intro-fiche">
+                        <?php (print $node->field_intro_fiche_formation[0]['view']); /* Inutile */?>
+                    </div>
+                <?php endif; ?>
+                
+                
+                        <div class="content">
+                <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
+                            
+                            <?php print $node->content['webform']['#value']; ?>
+                </div>
 
-  <div class="content">
-    <table>
-    <tr>
-    <td><?php print $node->content['body']['#value']; ?></td>
-    </tr>
-    <tr>
-    <td><?php print $node->content['webform']['#value']; ?></td>
-    </tr>
-    </table>
-  </div>
-
-    <?php if ($links): ?>
-    <div class="links">&raquo; <?php print $links; ?></div>
-  <?php endif; ?>
-
-     <?php if ($terms) { ?>
-    <span class="taxonomy"><?php print $terms?></span>
-  <?php }; ?>
-
-</div>
-<!--______________COLONNE 3________________ -->
-        <div id="colonne-3" class="col3_layout_200_590_200">
-     <?php print $picture; ?>
-
-
-
-    <div class="content">
-
-        <br clear="all"/>
-           <?php if ($node->field_vue_actus_lycee[0]['view']): ?>
-        <div id="bloc-actu-lycee">
-           <?php  print $node->field_vue_actus_lycee[0]['view'] /*Vue actus du lycée*/ ?>
-        </div>
-           <?php endif;?>
-    </div>
-
-    <?php if ($terms): ?>
-      <div class="taxonomy"><?php //print $terms; ?></div>
-    <?php endif;?>
-
-   
-
-</div>
+                
+         
+         
+        </div><!--  /zone-1-->
 
 
-   
-  <?php if ($picture) { print $picture; }?>
-
-  <?php if ($page == 0) { ?>
-    <?php if ($title) { ?>
-      <h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2>
-    <?php }; ?>
-  <?php }; ?>
-
-<div class="clear-block clear"></div>
-
+</div> <!-- /node-inner -->
 </div><!-- /node -->
